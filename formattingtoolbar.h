155c20fa53250525f7a455f8e189f4b0fe0eac40
Jon Crowe <jonc@ukfsn.org>
2010-04-06 14:57:56 +0100

    Formatting toolbar

---
+++ formattingtoolbar.h
@@ -0,0 +1,62 @@
+/****************************************************************************
+
+Copyright (C) 2010 Jonathan Crowe.
+
+This file is part of Hovel.
+
+Hovel is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hovel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hovel.  If not, see <http://www.gnu.org/licenses/>.
+
+****************************************************************************/
+
+#ifndef FORMATTINGTOOLBAR_H
+#define FORMATTINGTOOLBAR_H
+
+#include <QToolButton>
+#include <QFontComboBox>
+#include <QComboBox>
+
+namespace Hovel
+{
+
+	class FormattingToolBar : public QWidget
+	{
+		Q_OBJECT
+
+	public:
+		explicit FormattingToolBar(QWidget *parent = 0);
+
+	public slots:
+		void boldButtonPressed ( bool checked );
+		void italicButtonPressed ( bool checked );
+		void underlineButtonPressed ( bool checked );
+		void strikeThroughButtonPressed ( bool checked );
+
+	signals:
+		void boldToggled ( bool checked );
+		void italicToggled ( bool checked );
+		void underlineToggled ( bool checked );
+		void strikeThroughToggled ( bool checked );
+
+	private:
+		QToolButton		* _boldButton;
+		QToolButton		* _italicButton;
+		QToolButton		* _underlineButton;
+		QToolButton		* _strikeThroughButton;
+		QFontComboBox	* _fontComboBox;
+		QComboBox		* _fontSizeComboBox;
+	};
+
+}
+
+#endif // FORMATTINGTOOLBAR_H
